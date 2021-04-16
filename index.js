dcab5ce07d30f253b1f2fc496a178dc9d42fada0
Matias Reyes <matias_reyes@icloud.com>
2021-04-15 23:58:44 -0400

    Added info page at /info

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people`)
+    
+})
+
 
 const PORT = 3001
 app.listen(PORT)
