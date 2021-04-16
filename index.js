3cdf60f42820b3d5f1320a2c46d0a0f30d5ab77e
Matias Reyes <matias_reyes@icloud.com>
2021-04-15 23:47:33 -0400

    Server returns json list of people in contact sheet on port 3001

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        id: 1,
+        name: "Stella Adler",
+        number: "555-553-1234"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "555-253-6534"
+    },
+    {
+        id: 3,
+        name: "Maria Hill",
+        number: "555-234-4321"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
