1ac720331019095d13ca0d21f7e7c168b396584f
Matias Reyes <matias_reyes@icloud.com>
2021-04-16 00:16:18 -0400

    Server can now send individual person info at /api/persons/id

--- index.js
+++ index.js
@@ -34,6 +34,18 @@ app.get('/info', (request, response) => {
     response.send(info)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).json({
+            error: 'note does not exist with that id'
+        })
+    }
+})
+
 
 const PORT = 3001
 app.listen(PORT)
