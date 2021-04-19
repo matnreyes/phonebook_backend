0034936ca75a407ce799d25495042edafcdd29ac
Matias Reyes <matias_reyes@icloud.com>
2021-04-19 01:19:09 -0400

    Set port for Heroku

--- index.js
+++ index.js
@@ -97,6 +97,6 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
