327e2b3b8951118c01ad68f63ab18677ec7a0026
Matias Reyes <matias_reyes@icloud.com>
2021-04-19 02:10:33 -0400

    Reread instructions for exercise and had to restructure

--- frontend/README.md
+++
@@ -1,3 +0,0 @@
-# Basic phonebook React app
-I created a basic phonebook up with contact lookup.
-It uses the Axios library to pull data from a database of contacts. 
--- frontend/src/components/filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Filter = ({value, filter}) => (
-    <form>
-        <div>Filter names: <input value={value} onChange={filter}/></div>
-    </form> 
-)
-
-export default Filter
