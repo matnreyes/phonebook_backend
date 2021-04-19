9263b4d279e51fd90e6412ebd0524791754b1545
Matias Reyes <matias_reyes@icloud.com>
2021-04-19 02:32:14 -0400

    Connected frontend to backend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 morgan.token('content', (req, res) => JSON.stringify(req.body))
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :req[content-length] :req[header] :total-time ms :content'))
