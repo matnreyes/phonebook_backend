24f9e4a40b7519371496fc05512ab8d1075035d6
Matias Reyes <matias_reyes@icloud.com>
2021-04-26 00:54:30 -0400

    fixed styling

--- index.js
+++ index.js
@@ -14,100 +14,100 @@ app.use(morgan(':method :url :status :req[content-length] :req[header] :total-ti
 
 // Failsafe if frontend fails
 app.get('/', (request, response) => {
-    response.send('<h1>Something went wrong loading site</h1>')
+  response.send('<h1>Something went wrong loading site</h1>')
 })
 
 // Load contacts from server
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => { 
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    const opts = ({ runValidators: true, new: true, context: 'query' })
-  
-    Person.findByIdAndUpdate(request.params.id, person, opts)
-      .then(updatedPerson => {
-        response.json(updatedPerson)
-      })
-      .catch(error => next(error))
+  const opts = ({ runValidators: true, new: true, context: 'query' })
+
+  Person.findByIdAndUpdate(request.params.id, person, opts)
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    Person.find({}).then(persons => { 
-        const people = persons.length
-        const info = `<p>Phonebook has info for ${people} people</p><p>${Date()}</p>`
-        response.send(info)
-    })
+  Person.find({}).then(persons => {
+    const people = persons.length
+    const info = `<p>Phonebook has info for ${people} people</p><p>${Date()}</p>`
+    response.send(info)
+  })
 
 })
 
 // Load specific page for contact (JSON)
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then(person => {
-        if (!person) {
-            response.status(404).end()
-        }
-        response.json(person)
+      if (!person) {
+        response.status(404).end()
+      }
+      response.json(person)
     })
     .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(404).send({ error: 'maformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(404).send({ error: error.message})
-    }
+  if (error.name === 'CastError') {
+    return response.status(404).send({ error: 'maformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(404).send({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // Upload a new contact to DB
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.name || !body.number) {
-        response.status(400).json({
-            error: 'Contact is missing a field'
-        })
-    }
-
-    const newPerson = new Person({
-        name: body.name,
-        number: body.number
+  if (!body.name || !body.number) {
+    response.status(400).json({
+      error: 'Contact is missing a field'
     })
+  }
 
-    newPerson
+  const newPerson = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  newPerson
     .save()
     .then(savedPerson => savedPerson.toJSON())
     .then(savedAndFormattedPerson => {
-        response.json(savedAndFormattedPerson)
+      response.json(savedAndFormattedPerson)
     })
     .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
